6f9bec4b9d2e55aa27705dfbd9eb9591895c4080
chstaiger <staigerchristine@gmail.com>
2023-09-12 13:11:26 +0200

    Merge with micerocker; adjusting error handling for version.

--- webmice_handlers.R
+++ webmice_handlers.R
@@ -1,69 +1,141 @@
-pool_handler = function(.req, .res) {
-  poolJson <- ''
+#' End point handlers
+#'
+#' @param .req Required arguments
+#' @param .res Result
+#' @export
+pool_handler <- function(.req, .res) {
+  pool_json <- ""
   json_payload <- as.character(.req$parameters_query[["payload"]])
-  if (length(json_payload) == 0L) {raise(HTTPError$bad_request())}
+  if (length(json_payload) == 0L) {
+    raise(HTTPError$bad_request())
+  }
   params <- json_to_parameters(json_payload)
-  if(is.null(params$data)) {poolJson <- "Error: no data"}
-  if(poolJson == ''){
+  if (is.null(params$data)) {
+    pool_json <- "Error: no data"
+  }
+  if (pool_json == "") {
     print("DEBUG: Calling pool.table (requires 3.16.4)")
-    poolJson <- call_pool(params$data)
+    pool_json <- call_pool(params$data)
   }
-  .res$set_body(poolJson)
+  .res$set_body(pool_json)
   .res$set_content_type("text/plain")
 }
 
-fit_handler = function(.req, .res) {
-  fitJson <- ''
+#' @rdname pool_handler
+#' @export
+fit_handler <- function(.req, .res) {
+  fit_json <- ""
   json_payload <- as.character(.req$parameters_query[["payload"]])
-  # if answers are copied straight from the Swagger interface, there are too many backslashes
+  # if answers are copied straight from the Swagger interface,
+  # there are too many backslashes
   # json_payload <- gsub('\\\\', '', input)
-  if (length(json_payload) == 0L) {raise(HTTPError$bad_request())}
-  params <- json_to_parameters(json_payload)
-  if(is.null(params$data)) {fitJson <- "Error: no data"}
-  if(is.null(params$model)) {fitJson <- "Error: no model"}
-  if(is.null(params$formula)) {fitJson <- "Error: no formula"}
-
-  if(fitJson == ''){
-    print("DEBUG: Calling with (fitting function)")
-    fitJson <- call_with(params$data, params$model, params$formula)
+  if (length(json_payload) == 0L) {
+    raise(HTTPError$bad_request())
   }
-  .res$set_body(fitJson)
+  params <- json_to_parameters(json_payload)
+  if (is.null(params$data)) {
+    fit_json <- "Error: no data"
+  }
+  if (is.null(params$model)) {
+    fit_json <- "Error: no model"
+  }
+  if (is.null(params$formula)) {
+    fit_json <- "Error: no formula"
+  }
+
+  if (fit_json == "") {
+    print("DEBUG: Calling with (fitting function)")
+    fit_json <- call_with(params$data, params$model, params$formula)
+  }
+  .res$set_body(fit_json)
   .res$set_content_type("text/plain")
 }
 
-impute_longfmt_handler = function(.req, .res) {
+#' @rdname pool_handler
+#' @export
+impute_longfmt_handler <- function(.req, .res) {
   json_payload <- as.character(.req$parameters_query[["payload"]])
-  
-  if (length(json_payload) == 0L) {raise(HTTPError$bad_request())}
+  impute <- list()
+  impute$result <- ""
+  impute$error <- ""
+  check <- TRUE
+
+  if (length(json_payload) == 0L) {
+    check <- FALSE
+    impute$error <- "No input"
+  } 
   # check if convertible to json
   params <- json_to_parameters(json_payload)
-
-  # impute function needs data, maxit, m, seed
-  if(is.null(params$data)) {raise(HTTPError$not_acceptable())}
-  if(is.null(params$maxit)) {raise(HTTPError$not_acceptable())}
-  if(is.null(params$m)) {raise(HTTPError$not_acceptable())}
-  if(is.null(params$seed)) {raise(HTTPError$not_acceptable())}
-  
-  print("DEBUG: Calling mice")
-  imp <- call_mice(params)
-
-  if(is.null(imp$error)){
-    .res$set_body(imp_result_long_fmt(imp))
-  } else{
-    .res$set_body(toJSON(imp))
+  # impute function needs data
+  if (is.null(params$data)) {
+      check <- FALSE
+      impute$error <- "No data"
   }
-  .res$set_content_type("text/plain")
+  if (is.null(params$maxit)) {
+      check <- FALSE
+      impute$error <- "No maxit"
+  }
+  if (is.null(params$seed)) {
+      check <- FALSE
+      impute$error <- "No seed"
+  }
+  if (!check) {
+    impute$success <- FALSE
+    .res$set_body(toJSON(impute))
+    .res$set_content_type("text/plain")
+    return()
+  }
+  else {
+    if (is.null(params$m)) {
+      params$m <- 5
+    }
+    print("DEBUG: Calling mice")
+    imp <- call_mice(params)
+    if (is.null(imp$error)) {
+      impute$success <- TRUE
+      impute$result <- imp_result_long_fmt(imp)
+    } else {
+      impute$success <- FALSE
+      impute$error <- imp$error
+    }
+    .res$set_body(toJSON(impute))
+    .res$set_content_type("text/plain")
+  }
 }
 
-example_data_handler = function(.req, .res) {
+#' @rdname example_data_handler
+#' @export
+example_data_handler <- function(.req, .res) {
   example_name <- as.character(.req$parameters_query[["name"]])
   .res$set_body(example_data_to_json(example_name))
   .res$set_content_type("text/plain")
 }
 
-mice_version_handler = function(.req, .res) {
+#' @rdname mice_version_handler
+#' @export
+mice_version_handler <- function(.req, .res) {
   version <- list()
-  version$mice <- sessionInfo("mice")$otherPkgs$mice$Version
+  version$success <- TRUE
+  version$error <- ""
+
+  result <- tryCatch(
+    {
+      version$result <- sessionInfo("mice1")$otherPkgs$mice$Version
+    },
+    warning = function(w) {
+      res <- list()
+      res$success <- FALSE
+      res$result <- ""
+      res$error <- w$message
+      return(res)
+    }
+  )
+  print("RESULT")
+  print(result)
+
+  if (typeof(result) == "list") {
+    version <- result
+  }
   .res$set_body(toJSON(version))
   .res$set_content_type("text/plain")
 }
