de660178cd52b3704cc3eb9210fc8a9a70e34085
chstaiger <staigerchristine@gmail.com>
2023-09-13 19:53:53 +0200

    Adding imports.R

---
+++ imports.R
@@ -0,0 +1,6 @@
+#' @importFrom digest          digest
+#' @importFrom jsonlite        fromJSON toJSON
+#' @importFrom mice            complete mice pool.table
+#' @importFrom RestRserve      HTTPError raise
+#' @importFrom utils           packageVersion read.csv sessionInfo
+NULL
